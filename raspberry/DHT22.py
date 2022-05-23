4b83532a252d9aa1d493be2014ee90ac92581903
bkissinger <benjamin@kissinger.at>
2022-05-23 13:08:01 +0200

    Python-Skript ==> MQTT publisher

--- raspberry/DHT22.py
+++ raspberry/DHT22.py
@@ -2,7 +2,8 @@
 import time, adafruit_dht, board
 import mariadb
 import sys
-import time
+import paho.mqtt.client as mqttClient
+
 
 # Connect to MariaDB Platform
 try:
@@ -38,28 +39,68 @@ dhtDevice = adafruit_dht.DHT22(dhtboard, use_pulseio=False)
 # Standard is, but not working on the raspberry pi boards
 #dhtDevice = adafruit_dht.DHT22(dhtboard)
 
-while True:
-  try:
+
+try:
     # Print the values to the serial port
     temperature, humidity = dhtDevice.temperature, dhtDevice.humidity
     print("Temperature: {:.1f} °C  Humidity: {:.1f} %".format(temperature, humidity))
 
     cur.execute(
-      "INSERT INTO Messung (Temperatur, Luftfeuchtigkeit) VALUES (?, ?)",
-      (temperature, humidity))
+        "INSERT INTO Messung (Temperatur, Luftfeuchtigkeit) VALUES (?, ?)",
+        (temperature, humidity))
     # conn.commit()
 
 
-  except RuntimeError as error:
+except RuntimeError as error:
     # Errors happen fairly often, DHT's are hard to read, just keep going
     time.sleep(2.0)
-    # Print the values to the serial port
     temperature, humidity = dhtDevice.temperature, dhtDevice.humidity
-    print("Temperature: {:.1f} °C  Humidity: {:.1f} %".format(temperature, humidity))
 
-  except mariadb.Error as e:
-      print(f"Error: {e}")
-  time.sleep(10)
+except mariadb.Error as e:
+    print(f"Error: {e}")
+time.sleep(2)
 
 conn.close()
 
+t = str(temperature)
+h = str(humidity)
+msg = t + "-" + h
+
+def on_connect(client, userdata, flags, rc):
+
+    if rc == 0:
+
+        print("Connected to broker")
+
+        global Connected                #Use global variable
+        Connected = True                #Signal connection
+
+    else:
+
+        print("Connection failed")
+
+Connected = False   #global variable for the state of the connection
+
+broker_address= "localhost"
+port = 1883
+user = "WRP"
+password = ""
+
+client = mqttClient.Client("Raspi")               #create new instance
+client.username_pw_set(user, password=password)    #set username and password
+client.on_connect= on_connect                      #attach function to callback
+client.connect(broker_address, port=port)          #connect to broker
+
+client.loop_start()        #start the loop
+
+while Connected != True:    #Wait for connection
+    time.sleep(0.1)
+
+try:
+
+    client.publish("Home/WRP",msg)
+
+except KeyboardInterrupt:
+
+    client.disconnect()
+    client.loop_stop()
